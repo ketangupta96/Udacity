9f71b76a71dab4b6147c76954bc1c0553854209f
ketangupta96 <ketan.gupta50@yahoo.co.in>
2016-03-05 14:53:02 +0530

    Create Symmetric Square.py

---
+++ Intro-To-Computer-Science/Lesson-3-PSet/Symmetric Square.py
@@ -0,0 +1,49 @@
+# A list is symmetric if the first row is the same as the first column,
+# the second row is the same as the second column and so on. Write a
+# procedure, symmetric, which takes a list as input, and returns the
+# boolean True if the list is symmetric and False if it is not.
+def symmetric(p):
+    i = 0
+    n = len(p)
+    for e in p:
+        if n == len(p[0]):        
+            while i < n:
+                j = 0
+                while j < n:
+                    if p[i][j] == p[j][i]:
+                        j += 1
+                    else:
+                        return False
+                i += 1
+        else:
+            return False
+    return True
+    # Your code here
+
+#print symmetric([[1, 2, 3],
+#                [2, 3, 4],
+#                [3, 4, 1]])
+#>>> True
+
+#print symmetric([["cat", "dog", "fish"],
+#                ["dog", "dog", "fish"],
+#                ["fish", "fish", "cat"]])
+#>>> True
+
+#print symmetric([["cat", "dog", "fish"],
+#                ["dog", "dog", "dog"],
+#                ["fish","fish","cat"]])
+#>>> False
+
+#print symmetric([[1, 2],
+#                [2, 1]])
+#>>> True
+
+#print symmetric([[1, 2, 3, 4],
+#                [2, 3, 4, 5],
+#                [3, 4, 5, 6]])
+#>>> False
+
+#print symmetric([[1,2,3],
+#                 [2,3,1]])
+#>>> False
